0322ffef505a2bd812d2eea73535d76cfcb04bbf
Misam <misamplus@gmail.com>
2020-06-11 23:45:18 +0430

    Fixed a bug

--- js/review.js
+++ js/review.js
@@ -1,39 +1,35 @@
-function newReview() {
-    const db = new DB(function() {
-        $(document).ready(function() {
-            try {
-                var card = db.getReview();
-                
-                $("#edit_link").attr("href", $("#edit_link").attr("href").replace("@", card.id));
-                $("#front").val(card.front);
-                autoGrow($("#front")[0]);
-                $("#back").val("");
-                autoGrow($("#back")[0]);
-                $("#note").val("");
-                autoGrow($("#note")[0]);
-        
-                $("#back").click(function () {
-                    $("#back").val(card.back);
-                    autoGrow($("#back")[0]);
-                });
+const db = new DB(function() {
+    $(document).ready(function() {
+        try {
+            var card = db.getReview();
             
-                $("#know").click(function () {
-                    db.increaseBox(card.id);
-                    db.save();
-                    newReview();
-                });
-            
-                $("#dont").click(function () {
-                    db.reset(card.id);
-                    db.save();
-                    newReview();
-                });
-            } catch(err) {
-                alert(err);
-                window.location.href = "./index.html";
-            }
-        });
-    });
-}
+            $("#edit_link").attr("href", $("#edit_link").attr("href").replace("@", card.id));
+            $("#front").val(card.front);
+            autoGrow($("#front")[0]);
+            $("#back").val("");
+            autoGrow($("#back")[0]);
+            $("#note").val("");
+            autoGrow($("#note")[0]);
 
-newReview();
+            $("#back").click(function () {
+                $("#back").val(card.back);
+                autoGrow($("#back")[0]);
+            });
+
+            $("#know").click(function () {
+                db.increaseBox(card.id);
+                db.save();
+                window.location.replace("./review.html");
+            });
+
+            $("#dont").click(function () {
+                db.reset(card.id);
+                db.save();
+                window.location.replace("./review.html");
+            });
+        } catch(err) {
+            alert(err);
+            window.location.replace("./index.html");
+        }
+    });
+});
